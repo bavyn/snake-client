8bf80d18c1c3dcb30f1d916a5dd9cd83a43236fc
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-10 17:40:52 -0700

    moved funcs from play to input

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
 const { connect } = require("./client");
 
-// setup interface to handle user input from stdin
-// allow us to listen for keyboard input and react to it
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); // event listener, a function called handleUserInput
-  return stdin;
-};
 
-// event listener for stdin.  listener will use function handleUserInout that runs when you receive input from keyboard
-// specifies what happens when data is received from stdin aka when a particular key is pressed on keyboard input 
-const handleUserInput = function (key) {
-  //const stdout = process.stdout;
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-}
-};
 
 
