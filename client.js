13cab8a14ce07bd1a37b7263d192cfe38c548df3
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-10 20:53:51 -0700

    moved constant variables over to constants file

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, port, playerName } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: "50541", // PORT number here,
+    host: IP, // IP address here,
+    port: port, // PORT number here,
   });
 
   // interpret incoming data as text
@@ -16,21 +17,23 @@ const connect = function () {
   },
   // register another event handler for the connect event
   conn.on("connect", () => {
-    conn.write("Name: KAR");
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 1000);
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 500);
-
+    conn.write(playerName);
     console.log("Successfully connected to game server");
   }));
   return conn;
 
 };
 
-// console.log("Connecting ...");
-// connect();
-
 module.exports = { connect };
+
+// if i wanted to implement set interval or timer, it would be as per below
+// conn.on("connect", () => {
+//   conn.write("Name: KAR");
+  // setTimeout(() => {
+  //   conn.write("Move: up");
+  // }, 1000);
+  // setInterval(() => {
+  //   conn.write("Move: up");
+  // }, 500);
+ // console.log()
+// })
---
+++ constants.js
@@ -0,0 +1,32 @@
+// connection info
+const IP = "165.227.47.243";
+const port = "50541";
+
+// player name
+const playerName = "Name: KAR";
+
+// move commands
+const MOVE_UP_KEY = 'w';
+const MOVE_DOWN_KEY = 's';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_RIGHT_KEY = 'd';
+
+// messages
+const j = "Say: hi";
+const k = "Say: bye";
+const l = "Say: sucka";
+const f = "Say: nice move";
+
+module.exports = {
+  IP,
+  port,
+  playerName,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  j,
+  k,
+  l,
+  f,
+};
